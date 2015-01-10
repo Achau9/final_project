b5c887ea05a350184de33cc51bc74a02420b6a51
Pomeroy Mohabir <pmohabir2@afsenyc.org>
2015-01-10 14:00:51 -0500

    Added

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'bundler'
 
-
 Bundler.require
 
-# require_relative ''
+require_relative 'lib/pinterest.rb'
 
 class MyApp < Sinatra::Base
 get '/' do
     erb :index
-  end
+end
 
-  
+post '/plastic' do
+  @plastic_results = Pinterest.new(params[:plastic])
+  erb :plastic_item
+end
 
+post '/cardboard' do
+  @plastic_results = Pinterest.new(params[:cardboard])
+  erb :cardboard_item
+end
 end
--- lib/pinterest.rb
+++ lib/pinterest.rb
@@ -1,19 +1,27 @@
-require "nokogiri"
+
 require "open-uri"
 
 class Pinterest 
 
-  attr_acessor = :url
-  
-  def initialize
-    html = open("https://www.pinterest.com/search/?q=cardboard")
-    cardboard = Nokogiri::HTML(html)
-    url = []
-    suggestion = cardboard.css(".pinHolder").each do |pin|
-      url << "https://www.pinterest.com"+pin.children[1].first[1]
+  attr_accessor :url
+
+  def initialize(search_query)
+    html = open("https://www.pinterest.com/search/?q=#{search_query}")
+    @cardboard = Nokogiri::HTML(html)
+    scrape_url
+    scrape_image
+
+  end
+
+  def scrape_url
+    @url = []
+    suggestion = @cardboard.css(".pinHolder").each do |pin|
+      @url << "https://www.pinterest.com"+pin.children[1].first[1]
     end
+  end
+
+  def scrape_image
+    #scrape the image
   end
 end
   
-puts url
-
