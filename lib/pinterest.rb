00a5eee58ed54e53c819a348747e534d2736d12d
TechJames <jli@afsenyc.org>
2015-01-17 10:11:03 -0500

    Scraped Image

--- lib/pinterest.rb
+++ lib/pinterest.rb
@@ -7,7 +7,7 @@ class Pinterest
 
   def initialize(search_query)
     html = open("https://www.pinterest.com/search/?q=#{search_query}")
-    @cardboard = Nokogiri::HTML(html)
+    @material = Nokogiri::HTML(html)
     scrape_url
     scrape_image
 
@@ -15,13 +15,16 @@ class Pinterest
 
   def scrape_url
     @url = []
-    suggestion = @cardboard.css(".pinHolder").each do |pin|
-      @url << "https://www.pinterest.com"+pin.children[1].first[1]
+    suggestion = @material.css(".pinHolder").each do |pin|
+    @url << "https://www.pinterest.com"+pin.children[1].first[1]
     end
   end
 
   def scrape_image
-    
+    @image = []
+    pic =  material.css(".pinHolder").each do |pin|
+    @image << pin.children.children.children.children.children[1].first[1]
   end
 end
-  
+
+
