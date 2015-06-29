7ab44a935b196e19f45d165376d50212d175f9bf
Alston Chau <Achau9@Alstons-MacBook-Pro.local>
2015-06-28 22:25:14 -0400

    Test One

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ post '/plastic' do
   @plastic_results = Pinterest.new(params[:plastic])
   erb :plastic_item
 end
-
+# AHHHH
 post '/cardboard' do
   @cardboard_results = Pinterest.new(params[:cardboard])
   erb :cardboard_item
